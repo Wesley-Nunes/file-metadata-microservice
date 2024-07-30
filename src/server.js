ff5f19c5cdd8af1200e1146f2b22c7e14eb5c484
Wesley-Nunes <wesnmonteiro@gmail.com>
2024-07-30 12:24:41 -0300

    feat: Make the view

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import multer from 'multer';
 import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || '127.0.0.1';
+const __dirname = import.meta.dirname;
 const upload = multer({ dest: 'uploads/' });
 
 app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(`${__dirname}/index.html`));
+});
 
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 	const response = {
